971b2f0b9e12e4d3f25dc9589d8c7e4fc5fcbe9a
falec <falec@NOTE-SAULO>
2024-09-09 18:41:46 -0300

    Add project files.

---
+++ MemoryBenchmark.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    public class MemoryBenchmark
+    {
+        int NumeroDeItens = 100;
+      
+        [Benchmark]
+        public string ConcatStringsUsando_StringJoin()
+        {
+            string result = "";
+            for (int i = 0; i < NumeroDeItens; i++)
+            {
+                result += "SauloLima.com.br " + i;
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public string ConcatenandoStringsCom_StringBuilder()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < NumeroDeItens; i++)
+            {
+                sb.Append("SauloLima.com.br " + i);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Benchmark;
+using BenchmarkDotNet.Running;
+
+Console.WriteLine("### Hello, BenchmarkDotNet  ###\n");
+Console.WriteLine("Pressione algo para iniciar\n");
+Console.ReadLine();
+
+var summary = BenchmarkRunner.Run<MemoryBenchmark>();
